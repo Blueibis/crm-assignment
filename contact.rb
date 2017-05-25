31de147bae1f44869d63342bbc77c6611d23bafc
Blueibis <jennie.li.2011@gmail.com>
2017-05-24 21:42:37 -0400

    complete crm.rb tested

--- contact.rb
+++ contact.rb
@@ -1,91 +1,110 @@
 class Contact
-  attr_reader :id                                                       # => nil
-  attr_accessor :first_name, :last_name, :email, :note                  # => nil
-  @@contacts = []                                                       # => []
-  @@id = 1                                                              # => 1
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :email, :note
+  @@contacts = []
+  @@id = 1
   # This method should initialize the contact's attributes
   def initialize(first_name, last_name, email, note = "I'm a contact")
-    @first_name = first_name                                            # => "Jennie",        "mouse"
-    @last_name = last_name                                              # => "Li",            "keyboard"
-    @email = email                                                      # => "email",         "emaiiemail"
-    @note = note                                                        # => "I'm a contact", "I'm a contact"
-    @id = @@id                                                          # => 1,               2
-    @@id += 1                                                           # => 2,               3
-  end                                                                   # => :initialize
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+    @id = @@id
+    @@id += 1
+  end
 
   # This method should call the initializer,
   # store the newly created contact, and then return it
-  def self.create(first_name, last_name, email)
-    new_contact = Contact.new(first_name, last_name, email)  # => #<Contact:0x005620c9919408 @first_name="Jennie", @last_name="Li", @email="email", @note="I'm a contact", @id=1>,   #<Contact:0x005620c99137d8 @first_name="mouse", @last_name="keyboard", @email="emaiiemail", @note="I'm a contact", @id=2>
-    @@contacts << new_contact                                # => [#<Contact:0x005620c9919408 @first_name="Jennie", @last_name="Li", @email="email", @note="I'm a contact", @id=1>], [#<Contact:0x005620c9919408 @first_name="Jennie", @last_name="Li", @email="email", @note="I'm a contact", @id=1>, #<Contact:0x005620c99137d8 @first_name="mouse", @last_name="keyboard", @email="emaiiemail", @note="I'm a contact", @id=2>]
-    new_contact                                              # => #<Contact:0x005620c9919408 @first_name="Jennie", @last_name="Li", @email="email", @note="I'm a contact", @id=1>,   #<Contact:0x005620c99137d8 @first_name="mouse", @last_name="keyboard", @email="emaiiemail", @note="I'm a contact", @id=2>
-  end                                                        # => :create
+  def self.create(first_name, last_name, email, note = "I'm a contact")
+    new_contact = Contact.new(first_name, last_name, email, note = "I'm a contact")
+    @@contacts << new_contact
+    new_contact
+  end
 
   # This method should return all of the existing contacts
   def self.all
-    @@contacts  # => [#<Contact:0x005620c9919408 @first_name="Jennie", @last_name="Li", @email="email", @note="I'm a contact", @id=1>, #<Contact:0x005620c99137d8 @first_name="mouse", @last_name="keyboard", @email="emaiiemail", @note="I'm a contact", @id=2>], [#<Contact:0x005620c9919408 @first_name="Jennie", @last_name="Li", @email="email", @note="I'm a contact", @id=1>, #<Contact:0x005620c99137d8 @first_name="mouse", @last_name="keyboard", @email="emaiiemail", @note="I'm a contact", @id=2>]
-  end           # => :all
+    @@contacts
+  end
 
   # This method should accept an id as an argument
   # and return the contact who has that id
   def self.find(id)
-    result = nil                            # => nil
-    @@contacts.each do |contact|            # => [#<Contact:0x005620c9919408 @first_name="Jennie", @last_name="Li", @email="email", @note="I'm a contact", @id=1>, #<Contact:0x005620c99137d8 @first_name="mouse", @last_name="keyboard", @email="emaiiemail", @note="I'm a contact", @id=2>]
-      result = contact if contact.id == id  # => nil, #<Contact:0x005620c99137d8 @first_name="mouse", @last_name="keyboard", @email="emaiiemail", @note="I'm a contact", @id=2>
-    end                                     # => [#<Contact:0x005620c9919408 @first_name="Jennie", @last_name="Li", @email="email", @note="I'm a contact", @id=1>, #<Contact:0x005620c99137d8 @first_name="mouse", @last_name="keyboard", @email="emaiiemail", @note="I'm a contact", @id=2>]
-    result                                  # => #<Contact:0x005620c99137d8 @first_name="mouse", @last_name="keyboard", @email="emaiiemail", @note="I'm a contact", @id=2>
-  end                                       # => :find
+    @@contacts.each do |contact|
+      result = contact if contact.id == id
+    end
+    result
+  end
 
   # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
   def update(attribute, value)
-    case attribute              # => "first_name"
+    case attribute
     when "first_name"
-      self.first_name = value   # => "JenJen"
+      self.first_name = value
     when "last_name"
       self.last_name = value
     when "email"
       self.email = value
     when "note"
       self.note = value
-    end                         # => "JenJen"
-  end                           # => :update
+    end
+  end
 
   # This method should work similarly to the find method above
   # but it should allow you to search for a contact using attributes other than id
   # by specifying both the name of the attribute and the value
   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
   def self.find_by(attribute, value)
-    @@contacts.select do |contact|
+    result = @@contacts.select do |contact|
       contact.send(attribute) == value
     end
-  end                                   # => :find_by
+    result
+  end
 
   # This method should delete all of the contacts
   def self.delete_all
     @@contacts = []
-  end                  # => :delete_all
+  end
 
   def full_name
-    "#{first_name} #{last_name}"  # => "JenJen Li"
-  end                             # => :full_name
+    "#{first_name} #{last_name}"
+  end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
     @@contacts.delete(self)
-  end                        # => :delete
+  end
 
+  def self.delete_by_id(eyed)
+    @@contacts.each do |contact|
+      contact.delete if contact.id == eyed
+    end
+  end
+
+  def self.delete_by_attribute(attribute, value)
+    @@contacts.each do |contact|
+      contact.delete if contact.send(attribute) == value
+    end
+  end
+
+  def self.update(name, attribute, value)
+      @@contacts.each do |contact|
+      contact.first_name == name
+      case attribute
+        when "first_name"
+          contact.first_name = value
+        when "last_name"
+          contact.last_name = value
+        when "email"
+          contact.email = value
+        when "note"
+          contact.note = value
+        end
+      end
+  end
   # Feel free to add other methods here, if you need them.
 
-end  # => :delete
-
-jennie = Contact.create("Jennie", "Li", "email")           # => #<Contact:0x005620c9919408 @first_name="Jennie", @last_name="Li", @email="email", @note="I'm a contact", @id=1>
-mouse = Contact.create("mouse", "keyboard", "emaiiemail")  # => #<Contact:0x005620c99137d8 @first_name="mouse", @last_name="keyboard", @email="emaiiemail", @note="I'm a contact", @id=2>
-Contact.all                                                # => [#<Contact:0x005620c9919408 @first_name="Jennie", @last_name="Li", @email="email", @note="I'm a contact", @id=1>, #<Contact:0x005620c99137d8 @first_name="mouse", @last_name="keyboard", @email="emaiiemail", @note="I'm a contact", @id=2>]
-Contact.find(2)                                            # => #<Contact:0x005620c99137d8 @first_name="mouse", @last_name="keyboard", @email="emaiiemail", @note="I'm a contact", @id=2>
-Contact.all                                                # => [#<Contact:0x005620c9919408 @first_name="Jennie", @last_name="Li", @email="email", @note="I'm a contact", @id=1>, #<Contact:0x005620c99137d8 @first_name="mouse", @last_name="keyboard", @email="emaiiemail", @note="I'm a contact", @id=2>]
-jennie.update("first_name", "JenJen")                      # => "JenJen"
-jennie.full_name                                           # => "JenJen Li"
+end
--- crm.rb
+++ crm.rb
@@ -1,40 +1,104 @@
+require_relative 'contact.rb'  # => true
 class CRM
 
-  def initialize
-
-  end
+  def initialize(name)
+    @name = name                                 # => "My New App"
+    puts "Okay, this CRM has the name #{@name}"  # => nil
+  end                                            # => :initialize
 
   def main_menu
-
-  end
+    while true # repeat indefinitely
+        print_main_menu
+        user_selected = gets.to_i
+        call_option(user_selected)
+      end
+  end                               # => :main_menu
 
   def print_main_menu
+    puts '[1] Add a new contact'
+    puts '[2] Modify an existing contact'
+    puts '[3] Delete a contact'
+    puts '[4] Display all the contacts'
+    puts '[5] Search by attribute'
+    puts '[6] Exit'
+    puts 'Enter a number: '
+  end                                      # => :print_main_menu
 
-  end
+  def call_option(user_selected)
+    case user_selected
+      when 1 then add_new_contact
+      when 2 then modify_existing_contact
+      when 3 then delete_contact
+      when 4 then display_all_contacts
+      when 5 then search_by_attribute
+      when 6 then exit
+    end
+  end                                      # => :call_option
 
-  def call_option
-
-  end
 
   def add_new_contact
-
-  end
+    print 'Enter First Name: '
+    first_name = gets.chomp
+    print 'Enter Last Name: '
+    last_name = gets.chomp
+    print 'Enter Email Address: '
+    email = gets.chomp
+    print 'Enter a Note: '
+    note = gets.chomp
+    Contact.create(first_name, last_name, email, note)
+  end                                                   # => :add_new_contact
 
   def modify_existing_contact
-
-  end
+    puts "Who are you updating?"
+    name = gets.chomp
+    name.capitalize!
+    puts "Which attribute are you changing?"
+    attribute = gets.chomp
+    attribute.downcase!
+    puts "And what are you changing it to?"
+    value = gets.chomp
+    Contact.update(name, attribute, value)
+    puts "Changes made"
+  end  # => :modify_existing_contact
 
   def delete_contact
-
-  end
+    puts "Are you removing a contact by their ID or something else?"
+    result = gets.chomp
+    result.downcase!
+    if result == "id"
+      puts "Enter the id now"
+      eyed = gets.to_i
+      Contact.delete_by_id(eyed)
+      puts "Deleted #{Contact.find(eyed)}from contact list."
+    else
+      puts "What attribute do you want to search by?\n[first_name]\n[last_name]\n[email]\n[note]"
+      attribute = gets.chomp
+      puts "Who are you searching for?"
+      value = gets.chomp
+      value.capitalize!
+      Contact.delete_by_attribute(attribute, value)
+      puts "#{Contact.find_by(attribute, value)} has been deleted from contact list."
+    end
+  end                                                                                              # => :delete_contact
 
   def display_all_contacts
-    
-  end
+    puts Contact.all.inspect
+  end                       # => :display_all_contacts
 
   def search_by_attribute
-    
-  end
+    puts "What attribute do you want to search by?\n[first_name]\n[last_name]\n[email]\n[note]"
+    attribute = gets.chomp
+    puts "Who are you searching for?"
+    value = gets.chomp
+    value.capitalize!
+    Contact.find_by(attribute, value)
+    puts Contact.find_by(attribute, value)
+  end                                                                                            # => :search_by_attribute
 
 
-end
+end                                                        # => :search_by_attribute
+
+                          # => #<CRM:0x005560839ad768 @name="My New App">
+
+
+# >> Okay, this CRM has the name My New App
